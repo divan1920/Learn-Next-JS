d57a1cf10c18a2f36162c48af25718e375ddafd4
divan1920 <divanmunafsalimsha@gmail.com>
2023-11-16 16:58:19 +0530

    First commit with bootstrapped code

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+interface IPageProps {}
+
+const Page: React.FunctionComponent<IPageProps> = (props) => {
+  return <p>Dashboard Page</p>;
+};
+
+export default Page;
