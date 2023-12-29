2439d5ffd3500491fb7f7af16c6ca827869b840f
divan1920 <divanmunafsalimsha@gmail.com>
2023-12-29 16:33:09 +0530

    LN: chapter16 and chapter15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 
 const FormSchema = z.object({
   id: z.string(),
@@ -70,5 +72,27 @@ export async function deleteInvoice(id: string) {
     return { message: "Deleted Invoice." };
   } catch (error) {
     return { message: "Database Error: Failed to Delete Invoice." };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    console.log("hello");
+    const res = await signIn("credentials", formData);
+    console.log(res);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          console.log(error);
+          return "Something went wrong.";
+      }
+    }
+    throw error;
   }
 }
